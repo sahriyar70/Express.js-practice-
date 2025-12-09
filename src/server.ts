05dbdf35510895114ad056a63379c9de8382cd31
Al Sahriyar <alsahriyar229@gmail.com>
2025-12-09 09:29:02 +0600

    moduler pattern

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,15 @@
 import express, { NextFunction, Request, Response } from "express"
-import {Pool} from "pg"
-import dotenv from 'dotenv'
-import path from 'path'
 
-dotenv.config({path: path.join(process.cwd(), '.env')})
+import config from "./config";
+import initDB, { pool } from "./config/db";
+
+
+
 
 const app = express()
-const port = 3000
+const port = config.port
 
-const pool = new Pool({
-    connectionString : `${process.env.CONACTION_STRING}`
-});
 
-const initDB = async ()=>{
-    await pool.query(`
-        CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(50) UNIQUE NOT NULL,
-    age INT,
-    phone VARCHAR(15),
-    address TEXT,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-);
-
-        
-        `)
-        await pool.query(`
-          CREATE TABLE IF NOT EXISTS todos(
-          id SERIAL PRIMARY KEY ,
-          user_id INT REFERENCES users(id) ON DELETE CASCADE,
-          title VARCHAR(200) NOT NULL ,
-          description TEXT,
-          completed BOOLEAN DEFAULT  false ,
-          due_date DATE ,
-          created_at  TIMESTAMP DEFAULT NOW (),
-          uapdate_at  TIMESTAMP DEFAULT NOW () 
-
-          )
-          
-            `)
-}
 initDB()
 
 app.use(express.json())
